1f4aceb4ae1cfa28749cdc7b193a1aa5b8649221
presslord <vad311205@gmail.com>
2026-09-10 10:26:32 +0300

    first commit

---
+++ app/first/page.tsx
@@ -0,0 +1,10 @@
+export default function FirstPage() {
+  return (
+    <>
+    <div className="text-center">
+      <span className="text-3xl">Кошка</span>
+    </div>
+    <p className="text-justify">Важную часть моей жизни с недавних пор занимает это пока что маленькое чудо, которое порой переворачивает вещи в доме.</p>
+    </>
+  );
+}
---
+++ components/my/Head.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+
+export default function Head() {
+  const router = useRouter();
+  return (
+  <div className="pb-10">
+    <div className="flex justify-between rounded-xl p-4 backdrop-blur-xs shadow-lg items-center pb-2 border text-xl">
+      <button className="hover:cursor-pointer hover:scale-105" onClick={() => router.push('/')}>Главная</button>
+      <button className="hover:cursor-pointer hover:scale-105" onClick={() => router.push('/first')}>Кадр #1</button>
+      <button className="hover:cursor-pointer hover:scale-105" onClick={() => router.push('/second')}>Кадр #2</button>
+      <button className="hover:cursor-pointer hover:scale-105" onClick={() => router.push('/thrird')}>Кадр #3</button>
+      </div>
+  </div>
+  )
+}
