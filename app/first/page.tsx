6f5cb66b4c52cfe6b9da9f85f46092cb0675ba58
presslord <vad311205@gmail.com>
2026-09-12 19:46:41 +0300

    firstfromlaptop

--- app/first/page.tsx
+++ app/first/page.tsx
@@ -1,10 +1,15 @@
 export default function FirstPage() {
   return (
     <>
-    <div className="text-center">
-      <span className="text-3xl">Кошка</span>
+    <div className="flex justify-center mx-auto">
+      <span className="text-3xl font-bold">Кошка</span>
     </div>
-    <p className="text-justify">Важную часть моей жизни с недавних пор занимает это пока что маленькое чудо, которое порой переворачивает вещи в доме.</p>
+    <p className="py-3 max-w-5xl mx-auto text-xl">Важную часть моей жизни с недавних пор занимает это пока что маленькое чудо, которое порой переворачивает вещи в доме. С каждым днем она все больше растет, и по сравнению с своим первым днем она стала совсем взрослой.</p>
+    <p className="py-3 max-w-5xl mx-auto text-xl">Наша Луна как оказалось очень разговорчивая кошка, и по каждому поводу ей есть что сказать. Вместе с этим она достаточно тактильная, ее можно и нужно брать на ручки и гладить. Но так происходит не всегда, можно просто не угадать ее настроение, отчего запросто нарваться на острые коготки</p>
+    <p className="py-3 max-w-5xl mx-auto text-xl">Совсем недавно у Луны даже выпал молочный зуб, прямо мне на руку. Было страшно что он не молочный</p>
+    <div className="justify-center flex">
+      <img src="vazhn" className="rounded-2xl "></img>
+    </div>
     </>
   );
 }
